613cc573c888c2df4270c004c4a4617b1ab36036
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-13 18:10:25 +0600

    created folder component and new components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,32 @@
 import classes from "./Navbar.module.scss";
 
+import logoImage from "../../assets/logo/mancho.webp";
+
 function Navbar() {
-  return ( 
+  return (
     <div className={classes.Navbar}>
-      
+      <div className={classes.background}>
+        <div className="container">
+          <div className={classes.content}>
+            <ul className={classes.list}>
+              <div className={classes.logo}>
+                <li className={classes.img__container}>
+                  <img src={logoImage}  alt="#"/>
+                </li>
+              </div>
+              <li>
+                Главная
+              </li>
+              <li>
+                Блог
+              </li>
+              <li>
+                Hub
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
