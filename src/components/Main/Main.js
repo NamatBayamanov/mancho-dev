c164e36550166bff8bce7d24e005bd4a671190e8
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-20 00:56:22 +0600

    created new files within components folder

---
+++ src/components/Main/Main.js
@@ -0,0 +1,46 @@
+import classes from "./Main.module.scss";
+
+function Main() {
+  return ( 
+    <div className={classes.Main}>
+    asfasdf jkjlhjhahjsfasfha  a
+    aksdlfha;sdf
+    adsklfha;sdf
+    asldfhasdf
+    <br/>
+    asfasdf jkjlhjhahjsfasfha  a
+    aksdlfha;sdf
+    adsklfha;sdf
+    asldfhasdf
+    <br/>
+    asfasdf jkjlhjhahjsfasfha  a
+    aksdlfha;sdf
+    adsklfha;sdf
+    asldfhasdf
+    <br/>
+    asfasdf jkjlhjhahjsfasfha  a
+    aksdlfha;sdf
+    adsklfha;sdf
+    asldfhasdf
+    <br/>
+    asfasdf jkjlhjhahjsfasfha  a
+    aksdlfha;sdf
+    adsklfha;sdf
+    asldfhasdf
+    <br/>
+    asfasdf jkjlhjhahjsfasfha  a
+    aksdlfha;sdf
+    adsklfha;sdf
+    asldfhasdf
+    <br/>
+    asfasdf jkjlhjhahjsfasfha  a
+    aksdlfha;sdf
+    adsklfha;sdf
+    asldfhasdf
+    <br/>
+
+    </div>
+  );
+}
+
+export default Main;
