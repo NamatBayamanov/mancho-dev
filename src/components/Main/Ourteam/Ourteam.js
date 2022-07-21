fa6bdf3153ce0e728b132d07ead0c176bb854a3a
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-21 20:50:40 +0600

    added new component

---
+++ src/components/Main/Ourteam/Ourteam.js
@@ -0,0 +1,12 @@
+import classes from "./Ourteam.module.scss";
+function Ourteam() {
+  return ( 
+    <div className={classes.Ourteam}>
+      <h2>
+        Наша команда
+      </h2>
+    </div>
+  );
+}
+
+export default Ourteam;
