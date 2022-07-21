6c169a93b14453569fcd4fd708cfa4addd7a9a51
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-21 20:50:29 +0600

    created new component

---
+++ src/components/Main/MorePhotos/MorePhotos.js
@@ -0,0 +1,12 @@
+import classes from "./MorePhotos.module.scss";
+function MorePhotos() {
+  return ( 
+    <div className={classes.MorePhotos}>
+      <a href="/">
+        Больше фотографий из жизни компании
+      </a>
+    </div>
+  );
+}
+
+export default MorePhotos;
