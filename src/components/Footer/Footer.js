9220ac71291f56707d28fe275527c25729502ac3
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-23 00:30:20 +0600

    create new component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import classes from "./Footer.module.scss";
+function Footer() {
+  return ( 
+    <div className={classes.Footer}>
+      <div className={classes.background}>
+        <div className="container">
+          <div className={classes.content}>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
