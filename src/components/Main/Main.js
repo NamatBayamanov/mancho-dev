b91f1c0bd846035b2832068751b15fff91c1ff21
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-26 10:51:28 +0600

    added change

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,4 @@
+import Header from "../Header/Header";
 import Company from "./Company/Company";
 import HowWeWork from "./HowWeWork/HowWeWork";
 import JoinUs from "./JoinUs/JoinUs";
@@ -13,6 +14,7 @@ import PhotosCompany from "./PhotosCompany/PhotosCompany";
 function Main() {
   return (
     <div className={classes.Main}>
+      <Header/>
       <Company />
 
       <OurProducts />
