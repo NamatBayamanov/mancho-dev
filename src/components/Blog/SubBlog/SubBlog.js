aecc563fd9c40dc258e7f2f3a04d49c3663c8eb6
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-26 10:50:53 +0600

    added new component

---
+++ src/components/Blog/SubBlog/SubBlog.js
@@ -0,0 +1,128 @@
+import classes from "./SubBlog.module.scss";
+import { useParams } from "react-router-dom";
+import { getProduct } from "../../../data/data";
+import { useState } from "react";
+
+
+function SubBlog() {
+
+  
+
+  const { blogId } = useParams();
+
+  const product = getProduct(blogId);
+
+  console.log(product);
+
+
+  return (
+    <div className={classes.SubBlog}>
+      <div className={classes.background}>
+
+      <div className={classes.title}>
+              <h2>{product.name} <span>{product.name3}</span> <span className={classes.span}>{product.name2}</span></h2>
+              <p>
+                {product.text}
+              </p>
+            </div>
+
+
+        <div className={classes.sub__container}>
+          <div className={classes.content}>
+            
+            <div className={classes.user}>
+              <div className={classes.user__width}>
+                <div className={classes.img__container}>
+                  <img alt="#" src={product.bro_1}/>
+                </div>
+                <div className={classes.user__name}>
+                  <h2>
+                    {product.user}
+                  </h2>
+                  <p>
+                    {product.spec}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+            <div className={classes.iframe}>
+              <iframe src={product.src} allow={product.allow} frameBorder={product.num} allowFullScreen >
+              Ваш браузер не поддерживает этот iFRAME тег. Пожалуйста
+              обновитесь!
+              </iframe>
+            </div>
+
+            <div className={classes.text}>
+              <p>
+                {product.firstText}
+              </p>
+              <h2>
+                {product.question}
+              </h2>
+              <p>
+                {product.secondText}
+              </p>
+              <p>
+                <span>
+                  {product.instance}
+                </span>
+                <br/>
+                {product.thirdText}
+              </p>
+              <p>
+                {product.fourthText}
+              </p>
+              <h2>
+                {product.question2}
+              </h2>
+              <ol>
+                <li>
+                  {product.list1}
+                </li>
+                <li>
+                  {product.list2}
+                </li>
+                <li>
+                  {product.list3}
+                </li>
+                <li>
+                  {product.list4}
+                </li>
+
+              
+              </ol>
+
+              <p>
+                {product.fivth}
+              </p>
+              <p>
+                {product.sixth}
+              </p>
+            </div>
+
+            <div className={classes.user}>
+              <div className={classes.user__width}>
+                <div className={classes.img__container}>
+                  <img alt="#" src={product.bro_1}/>
+                </div>
+                <div className={classes.user__name}>
+                  <h2>
+                    {product.user}
+                  </h2>
+                  <p>
+                    {product.spec}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SubBlog;
