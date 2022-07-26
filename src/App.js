ad3eb1a1654967998a6b6494e288c936b8a247cd
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-26 10:53:06 +0600

    added changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import classes from "./App.module.scss";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import Navbar from "./components/Navbar/Navbar";
+import Layout from "./components/Layout/Layout";
+
+import Mains from "./page/Mains";
+import Blogs from "./page/Blogs/Blogs";
+import SubBlog from "./components/Blog/SubBlog/SubBlog";
+
 
 function App() {
   return (
     <div className={classes.App}>
-      <Navbar/>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Layout>
+        <Routes>
+        <Route path="/" element={<Mains/>}/>
+          {/* <Route path="/mancho.dev" element={<Mains/>}/> */}
+          <Route path="/blog" element={<Blogs/>}/>
+          <Route path="/blog/:blogId" element={<SubBlog />}/>
+        </Routes>
+      </Layout>
     </div>
   );
 }
