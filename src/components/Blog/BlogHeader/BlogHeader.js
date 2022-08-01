e2e66cb481e6e9ce4d398ed5915decee6c7730e0
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-02 02:01:52 +0600

    added new component

--- src/components/Blog/BlogHeader/BlogHeader.js
+++ src/components/Blog/BlogHeader/BlogHeader.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import classes from "./BlogHeader.module.scss";
 
 function BlogHeader({name}) {
+
+  const [h2, setH2] = useState({
+    opacity: "0",
+  });
+
+  useEffect(() => {
+    setH2({
+      opacity: "9",
+      transition: `opacity ${2}s`,
+    });
+  }, []);
   return ( 
     <div className={classes.BlogHeader}>
-      <h2>
+      <h2 style={h2}> 
         {name}
       </h2>
     </div>
