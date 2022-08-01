9975c668b21543e5f0cbf32d0e39aabd072fe64f
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-02 02:00:39 +0600

    added new component

--- src/components/Navbar/NavItem/NavItem.js
+++ src/components/Navbar/NavItem/NavItem.js
@@ -2,9 +2,9 @@ import classes from "./NavItem.module.scss";
 import { NavLink } from "react-router-dom";
 import { useMatch } from "react-router-dom";
 
-function NavItem({ url, children }) {
+function NavItem({ url, children, bottom }) {
   return (
-    <li className={classes.NavItem}>
+    <li className={`${classes.NavItem} ${bottom ? classes.bottom : "" }`}>
       <NavLink className={`${useMatch(url) ? classes.active : null }`} to={url}>{children}</NavLink>
     </li>
   );
