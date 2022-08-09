b95a199cc6918af8c02309e83a70c84686d74152
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-10 00:21:56 +0600

    added some changes

---
+++ src/components/Main/JoinUs/Vacancies/OpenVacancies/OpenVacancies.js
@@ -0,0 +1,12 @@
+import classes from "./OpenVacancies.module.scss";
+function OpenVacancies() {
+  return (
+    <div className={classes.OpenVacancies}>
+      <div className={classes.background}>
+        <h2>Открытые вакансии</h2>
+      </div>
+    </div>
+  );
+}
+
+export default OpenVacancies;
