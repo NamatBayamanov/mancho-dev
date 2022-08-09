5eb208c0a6e406c80d4e8ef4a260416e227367f3
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-10 00:21:11 +0600

    added change

--- src/components/Main/JoinUs/Vacancies/Vacancies.js
+++ src/components/Main/JoinUs/Vacancies/Vacancies.js
@@ -9,7 +9,25 @@ import ListProducts from "../../ListProducts/ListProducts";
 import PhotosCompany from "../../PhotosCompany/PhotosCompany";
 import MorePhotos from "../../MorePhotos/MorePhotos";
 import HowWeWork from "../../HowWeWork/HowWeWork";
+
+
+import VacanciesItem from "./VacanciesItem/VacanciesItem";
+import OpenVacancies from "./OpenVacancies/OpenVacancies";
+import ListVacancies from "./ListVacancies/ListVacancies";
+import Request from "./Request/Request";
+import Picture from "./Picture/Picture";
+import OurContact from "../../OurContact/OurContact";
+
+
+import { TiArrowSortedDown } from "react-icons/ti";
+
+
 function Vacancies() {
+
+
+  console.log(classes);
+
+
   const [btn, setBtn] = useState({
     transform: `translate(${-10000 + "px"})`,
   });
@@ -22,14 +40,18 @@ function Vacancies() {
       });
     }, 700);
   }, []);
+
+  
+
   return (
     <div className={classes.Vacancies}>
-      <JoinUsView effect={btn} link_1="Открытые вакансии " link_2="Подробнее">
+      <JoinUsView effect={btn} link_1="Открытые вакансии " link_2="Подробнее" btns={<TiArrowSortedDown/>} >
         Присоединяйся к команде Mancho Devs
       </JoinUsView>
       <Company />
 
       <div className={classes.background}>
+
         <div className="container">
           <div className={classes.content}>
             <div className={classes.img__container}>
@@ -60,11 +82,20 @@ function Vacancies() {
           </div>
         </div>
       </div>
+
       <OurProducts/>
       <ListProducts/>
       <PhotosCompany/>
       <MorePhotos/>
       <HowWeWork/>
+      
+      <VacanciesItem/>
+
+      <OpenVacancies/>
+      <ListVacancies/>
+      <Request/>
+      <Picture/>
+      <OurContact/>
     </div>
   );
 }
