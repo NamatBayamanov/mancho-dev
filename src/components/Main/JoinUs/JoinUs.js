30602ac915681ce56526737c120a9527fbff0699
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-10 00:20:41 +0600

    added new attribute

--- src/components/Main/JoinUs/JoinUs.js
+++ src/components/Main/JoinUs/JoinUs.js
@@ -2,6 +2,8 @@
 import classes from "./JoinUs.module.scss";
 import JoinUsView from "./JoinUsView/JoinUsView";
 
+import { TiArrowSortedDown } from "react-icons/ti";
+
 function JoinUs() {
 
 
@@ -9,27 +11,8 @@ function JoinUs() {
 
 
   return (
-    // <div className={classes.JoinUs}>
-    //   <div className={classes.backdrop}></div>
-    //   <div className={classes.background}>
-    //     <div className="container">
-    //       <div className={classes.content}>
-    //         <div className={classes.content__width}>
-    //           <h2 style={h2} >Присоединяйся к команде Mancho Devs!</h2>
-    //           <p style={p}>
-    //             Мы всегда в поисках талантливых людей, готовых работать над
-    //             сложными и интересными задачами вместе с нами
-    //           </p>
-    //           <div className={classes.btn}>
-    //             <Link style={btn} to="/join-us">ПодробнееПодробнее</Link>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <JoinUsView text="Мы всегда в поисках талантливых людей, готовых работать над сложными и интересными задачами вместе с нами" link_1="Подробнее">
-      Присоединяйся к команде Mancho Devs!
+    <JoinUsView scrolls={<TiArrowSortedDown/>} text="Мы всегда в поисках талантливых людей, готовых работать над сложными и интересными задачами вместе с нами" link_1="Подробнее">
+      Присоединяйся к команде <span>Mancho Devs!</span>
     </JoinUsView>
   );
 }
