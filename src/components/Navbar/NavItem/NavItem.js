edf262c5f1ff42d24792524d17634fb42a35e031
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-10 09:20:16 +0600

    added change

--- src/components/Navbar/NavItem/NavItem.js
+++ src/components/Navbar/NavItem/NavItem.js
@@ -2,10 +2,10 @@ import classes from "./NavItem.module.scss";
 import { NavLink } from "react-router-dom";
 import { useMatch } from "react-router-dom";
 
-function NavItem({ url, children, bottom }) {
+function NavItem({ url, children, bottom, toggle }) {
   return (
     <li className={`${classes.NavItem} ${bottom ? classes.bottom : "" }`}>
-      <NavLink className={`${useMatch(url) ? classes.active : null }`} to={url}>{children}</NavLink>
+      <NavLink className={`${useMatch(url) ? classes.active : null }`} to={url} onClick={toggle} >{children}</NavLink>
     </li>
   );
 }
