03051e03dcb44c1bdf298a09c626368bcc4400c4
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-10 09:20:45 +0600

    added change

--- src/components/ui/Logo/Logo.js
+++ src/components/ui/Logo/Logo.js
@@ -1,14 +1,15 @@
 import classes from "./Logo.module.scss";
 
 import logoImage from "../../../assets/logo/mancho.webp";
+import { Link } from "react-router-dom";
 
-function Logo() {
+function Logo({ toggle }) {
   return (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} onClick={toggle}>
       <li className={classes.img__container}>
-        <a href="/">
+        <Link to="/">
           <img src={logoImage} alt="#" />
-        </a>
+        </Link>
       </li>
     </div>
   );
