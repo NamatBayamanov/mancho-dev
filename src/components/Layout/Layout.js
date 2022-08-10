fa420ac09ad3f3dc5ff4953ddadccbcbd694a36d
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-10 09:19:15 +0600

    added change

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,29 @@ import classes from "./Layout.module.scss";
 // import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import Tooltip from "../Tooltip/Tooltip";
+import Drawer from "../Drawer/Drawer";
+import { useEffect, useState } from "react";
 
 function Layout({children}) {
+
+
+  const [toggle, setToggle] = useState(true);
+
+  const Toggle = () => {
+    setToggle(!toggle);
+  }
+
+  
+
+  
+
+
+
   return ( 
     <main>
-      <Navbar/>
+      <Tooltip action={Toggle}/>
+      <Drawer state={toggle} action={Toggle} />
       {/* <Header/> */}
 
       <div className={classes.content}>
