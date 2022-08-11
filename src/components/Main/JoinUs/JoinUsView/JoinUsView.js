16e55dc6197baf3a5761cfb144a5baff6f3ae0e6
Namat Bayamanov <namataxvd15@gmail.com>
2022-08-11 18:25:41 +0600

    added changes

--- src/components/Main/JoinUs/JoinUsView/JoinUsView.js
+++ src/components/Main/JoinUs/JoinUsView/JoinUsView.js
@@ -1,9 +1,10 @@
 import classes from "./JoinUsView.module.scss";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // import { TiArrowSortedDown } from "react-icons/ti";
 
-
+import Media from "react-media";
+import { TiEqualsOutline } from "react-icons/ti";
 
 
 function JoinUsView({ children, text, link_1, link_2, effect, btns, scrolls }) {
@@ -58,10 +59,19 @@ function JoinUsView({ children, text, link_1, link_2, effect, btns, scrolls }) {
   }
 
   const Scroll3 = (event) => {
-    window.scrollTo(0,6143);
+    window.scrollTo(0,10000);
+  }
+  const Scroll4 = (event) => {
+    window.scrollTo(0,11400);
   }
 
+  const Scroll5 = (event) => {
+    window.scrollTo(0,11800);
+  }
 
+  const Scroll6 = (event) => {
+    window.scrollTo(0,6250);
+  }
   return (
     <div className={classes.JoinUsView}>
       <div className={classes.backdrop}></div>
@@ -73,9 +83,35 @@ function JoinUsView({ children, text, link_1, link_2, effect, btns, scrolls }) {
               <p style={p}>{text}</p>
               <article className={classes.links}>
                 <div className={classes.btn}>
-                  <Link style={btn} to="/join-us" onClick={Scroll3}>
+                  {/* <Link style={btn} to="/join-us" onClick={Scroll3}>
                     {link_1}
-                  </Link>
+                  </Link>; */}
+
+                  <Media queries={{
+                    small: "(min-width: 500px) and (max-width: 625px)",
+                    medium: "(min-width: 625px) and (max-width: 900px)",
+                    large: "(min-width: 901px) and (max-width: 904px)",
+                    large2: "(min-width: 905px) and (max-width: 4000px)",
+                  }}>
+
+                  {matches => (
+                    <Fragment>
+                      {matches.small && <Link style={btn} to="/join-us" onClick={Scroll3}>
+                    {link_1}
+                  </Link>}
+                  {matches.medium && <Link style={btn} to="/join-us" onClick={Scroll4}>
+                    {link_1}
+                  </Link>}
+                  {matches.large && <Link style={btn} to="/join-us" onClick={Scroll5}>
+                    {link_1}
+                  </Link>}
+                  {matches.large2 && <Link style={btn} to="/join-us" onClick={Scroll6}>
+                    {link_1}
+                  </Link>}
+                    </Fragment>
+                  )}
+
+                  </Media>
                 </div>
                 <p> 
                   <Link style={effect} to="/join-us">{link_2}</Link>
